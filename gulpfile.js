fbb261ab0c064eb280dbb574ae718b76703a2cfe
twstddev <twstd.dev@gmail.com>
2014-04-18 22:16:02 +0100

    Add gulp tasks loader

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
 var gulp = require( "gulp" );
 
 GLOBAL.plugins = require( "gulp-load-plugins" )();
+
+// load tasks form the gulp directory
+require( "gulp-load" )( gulp );
+gulp.loadTasks( __dirname );
