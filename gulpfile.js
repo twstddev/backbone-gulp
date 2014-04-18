01824620f4f02e850bf9b0aafc2cf42f19f21281
twstddev <twstd.dev@gmail.com>
2014-04-18 22:23:40 +0100

    Add livereload

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,17 @@ var gulp = require( "gulp" );
 
 GLOBAL.plugins = require( "gulp-load-plugins" )();
 
-// load tasks form the gulp directory
+GLOBAL.config = {
+	hotname : "localhost",
+	port : 3000,
+	base : "app",
+	dist : "dist"
+};
+
+// load tasks form the tasks directory
 require( "gulp-load" )( gulp );
 gulp.loadTasks( __dirname );
+
+gulp.task( "development", [
+	"livereload"
+] );
